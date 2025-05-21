7d08f05111537806b767f738f8590d4177299825
jiajiajia003 <yangmaoa01@gmail.com>
2025-05-20 21:10:42 -0700

    Apply progressive darkening effect where each interaction dardens the square by 10%

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function createGrid(para) {
         for (let j = 0; j < para; j++) {
             const singleDiv = document.createElement(`Div`);
             singleDiv.classList.add(`singleDiv`);
+            singleDiv.classList.add(`0`);
             rowDiv.appendChild(singleDiv);
         }
     }
@@ -16,16 +17,36 @@ function createGrid(para) {
     const allDivs = document.querySelectorAll(`.singleDiv`);
     allDivs.forEach((singleDiv) => {
         singleDiv.addEventListener("mouseover", () => {
-            singleDiv.setAttribute(`style`, `background: ${randomColor()};`);
+            const classNames = singleDiv.className
+            let numOfTouch = Number(classNames.split(` `)[1]);
+            
+            singleDiv.classList.remove(`${+numOfTouch}`);
+            if (numOfTouch === 0) {
+                singleDiv.setAttribute(`style`, `background: rgba(${generateRandomColor()}, ${generateRandomColor()}, ${generateRandomColor()}, ${0.1 * (++numOfTouch)});`);
+                singleDiv.classList.add(`${numOfTouch}`);
+            }
+            else if (numOfTouch < 10) {
+                numOfTouch += 1;
+                const currentColor = getComputedStyle(singleDiv).backgroundColor;
+                opacity = (0.1 * numOfTouch).toFixed(1);
+                newColor = currentColor.slice(0, -4) + opacity + `)`;
+                singleDiv.setAttribute(`style`, `background: ${newColor}`);
+                singleDiv.classList.add(`${numOfTouch}`);
+            }
     })})
 }
 
-function randomColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
+function generateRandomColor() {
+    const randomColorNumber = Math.floor(Math.random() * 256);
+    return randomColorNumber
 }
+
+// function randomColor() {
+//     const r = Math.floor(Math.random() * 256);
+//     const g = Math.floor(Math.random() * 256);
+//     const b = Math.floor(Math.random() * 256);
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
 
 const container = document.querySelector(`.container`);
 
